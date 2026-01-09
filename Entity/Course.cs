a90865a364c61ce9ae2766a0002d5f73d2028a96
mizanur0711 <mizanur0711@gmail.com>
2026-01-09 15:22:02 +0600

    added extra field course model in Entity

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -2,5 +2,11 @@ namespace Entity;
 
 public class Course
 {
-    
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string Instructor { get; set; }
+    public decimal Rating { get; set; }
+    public string Image { get; set; }
 }
