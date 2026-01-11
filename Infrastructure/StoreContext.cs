ed1c0b2310f6116df9b16ca483109ba1d01bb80b
mizanur0711 <mizanur0711@gmail.com>
2026-01-11 19:57:42 +0600

    constructor in DbContext

---
+++ Infrastructure/StoreContext.cs
@@ -0,0 +1,13 @@
+using Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Course>  Courses { get; set; }
+}
