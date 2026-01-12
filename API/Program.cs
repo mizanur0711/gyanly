0411c0eb971e29fe28b841b3498626e095d69728
mizanur0711 <mizanur0711@gmail.com>
2026-01-12 20:45:43 +0600

    dbContext used in startup class file

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(x =>
+    {
+        x.UseSqlite("Data Source=gyanly.db");
+    }
+);
 
 var app = builder.Build();
 
