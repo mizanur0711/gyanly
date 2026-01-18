e009dd8a76d126088250cb8cfa4602c3bd37b2f2
mizanur0711 <mizanur0711@gmail.com>
2026-01-18 16:50:25 +0600

    nav bar and its scss, downgraded to react 18, will update soon, icons and ant designed install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
 import LoginPage from './pages/Login';
-
+import Navigation from './components/Navigation';
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/detail" element={<DetailPage />} />
-      <Route path='/login' element={<LoginPage />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </>
   );
 }
 
