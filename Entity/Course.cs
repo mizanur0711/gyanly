2b03fbc3b3fe4910be679d3e75a0d09dfea7524e
mizanur0711 <mizanur0711@gmail.com>
2026-01-25 21:35:31 +0600

    adding more field for course

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -8,4 +8,7 @@ public class Course :  BaseEntity
     public string Instructor { get; set; }
     public decimal Rating { get; set; }
     public string Image { get; set; }
+    public string SubTitle { get; set; } 
+    public int Students { get; set; }
+    
 }
